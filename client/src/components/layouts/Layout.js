99444a6d838ee793fb98b22741a7328be9c76a55
TepsVie <teps.vie@yahoo.com>
2018-06-21 10:44:55 -1000

    push nav

--- client/src/components/layouts/Layout.js
+++ client/src/components/layouts/Layout.js
@@ -9,7 +9,7 @@ class Layout extends Component {
             <div>
 
                 <div>
-                  {/*   <nav class="navbar navbar-expand-md navbar-light bg-light">
+                    <nav class="navbar navbar-expand-md navbar-light bg-light">
                         <a class="navbar-brand" >MrJoints Spearfishing</a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -30,7 +30,7 @@ class Layout extends Component {
                                 </li>
                             </ul>
                         </div>
-                    </nav> */}
+                    </nav>
 
 
                     {/* <ul>
