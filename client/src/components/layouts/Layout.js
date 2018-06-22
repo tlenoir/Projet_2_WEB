7c2fd59c3859a0d2f3b54b8dd6a8b72192beb4ff
TepsVie <teps.vie@yahoo.com>
2018-06-22 07:59:37 -1000

    lol

--- client/src/components/layouts/Layout.js
+++ client/src/components/layouts/Layout.js
@@ -3,46 +3,15 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../../asset/logoicon.png';
 import ban from '../../asset/banniere.jpg';
-import './layout.css'
+import './layout.css';
+
 
 class Layout extends Component {
     render() {
         return (
-            <div /* className='container-fluid' */>
+            <div>
 
-
-
-                {/*  <div>
-                    <div class='sticky-top'>
-                        <nav className="navbar navbar-light navbar-expand-md sticky-top navigation-clean-button darkNavClass navbar-static-top">
-                            <div className="container-fluid">
-                                <a className="navbar-brand aNav"><Link to={'/'}>
-                                    <img className="img-fluid imgNav" src={logo} />Mr. Joints Spearfishing</Link></a>
-                                <button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
-                                    <span className="sr-only">Toggle navigation</span>
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse navCol" id="navcol-1">
-                                    <ul className="nav navbar-nav ml-auto">
-                                        <li className="nav-item" role="presentation">
-                                            <a className="nav-link colorNav">
-                                                <i className="icon fa fa-home"></i><Link to={'/'}>Home</Link></a>
-                                        </li>
-                                        <li className="nav-item" role="presentation">
-                                            <a className="nav-link colorNav"><Link to={'/Products'}>Product</Link></a>
-                                        </li>
-                                        <li className="nav-item" role="presentation">
-                                            <a className="nav-link colorNav"><Link to={'/Lifestyles'}>Lifestyle</Link></a>
-                                        </li>
-                                        <li className="nav-item" role="presentation">
-                                            <a className="nav-link colorNav"><Link to={'/about'}>Contact</Link></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div> */}
-                <div className="navbar navbar-expand-md navbar-dark sticky-top navBg">
+                <div className="navbar navbar-expand-md navbar-dark sticky-top navBg" id='global-nav'>
                     <div className="container-fluid navStyle"><span className="text-white d-md-none"><Link to={'/'}><img className="img-fluid imgNav" src={logo} />Mr. Joints Spearfishing</Link></span><button className="btn btn-link navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav"><span className="navbar-toggler-icon"></span></button>
                         <div id="main-nav" className="navbar-collapse collapse">
                             <ul className="navbar-nav nav-fill w-100">
